03223b9bbce4f0f6ba2da4731a94c24a03a43220
pengYigao <lazysage@126.com>
2018-10-29 01:07:31 +0800

    use D3 to draw circle

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,7 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Button, Image } from '@tarojs/components'
 import { fetchWork, downloadWorkFile } from '../../utils/beevalley'
+import * as D3 from 'd3'
 import '@tarojs/async-await'
 import './index.scss'
 
@@ -16,7 +17,7 @@ export default class Index extends Component {
     this.state = {
       currentWork: {}
     }
-    this.apiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YmM0M2RiMmYxNzdjOTAwMDEzZTZkZTUiLCJyb2xlcyI6WyJXT1JLRVIiLCJSRVZJRVdFUiJdLCJpYXQiOjE1NDA2MjkzNjgsImV4cCI6MTU0MDcxNTc2OH0.vN2mXU5WeWOtck3QZ9U7duWdOnA5vbnLH5UL9Iem03c';
+    this.apiToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI1YmM0M2RiMmYxNzdjOTAwMDEzZTZkZTUiLCJyb2xlcyI6WyJXT1JLRVIiLCJSRVZJRVdFUiJdLCJpYXQiOjE1NDA3MTU5MDgsImV4cCI6MTU0MDgwMjMwOH0.26F_3tKRcTZc8KO-Xfxjk--ppa3EIraY4UWF3Zbzk5k';
   }
 
   componentWillMount() {
@@ -34,7 +35,7 @@ export default class Index extends Component {
   }
 
   nextWork = () => {
-    let nowWork = this.work.pop()
+    let nowWork = this.work.pop();
     if (this.screenWidth < 500) {
       let { imageWidth, imageHeight } = nowWork.meta;
       let ratio = imageWidth / imageHeight;
@@ -70,21 +71,34 @@ export default class Index extends Component {
 
   }
 
-  async componentDidMount() {
+  imgLoad = ()=> {
+    let svg = D3.select(".workImg")
+        .append("svg")
+        .attr("width", this.state.currentWork.meta.imageWidth)
+        .attr("height", this.state.currentWork.meta.imageHeight)
+        .on('click', function(){
+          console.log(D3.event)
+          svg.append("circle")
+          .attr("cx", D3.event.clientX)
+          .attr("cy", D3.event.clientY)
+          .attr("r", 10).attr("fill", "red");
+        });
+  }
 
+  async componentDidMount() {
     this.getWork();
+    const query = Taro.createSelectorQuery()
+      query
+      .select('.imgItem')
+      .boundingClientRect(rect => {
+        this.screenWidth = rect.width;
+      })
+      .exec()
     if (process.env.TARO_ENV === 'weapp') {
       // 这里 this.refs.input 访问的时候通过 `wx.createSeletorQuery` 取到的小程序原生组件
       console.log('weapp')
     } else if (process.env.TARO_ENV === 'h5') {
       // 这里 this.refs.input 访问到的是 `@tarojs/components` 的 `Input` 组件实例
-      const query = Taro.createSelectorQuery()
-      query
-        .select('.imgItem')
-        .boundingClientRect(rect => {
-          this.screenWidth = rect.width;
-        })
-        .exec()
     }
   }
 
@@ -93,11 +107,12 @@ export default class Index extends Component {
     // console.log( )
     return (
       <View className='index'>
-        <View className="imgItem" ref='imgRange'>
+        <View className="imgItem">
           {currentWork.src && (
-            <Image src={currentWork.src} style={`width:${currentWork.meta.imageWidth}px;height:${currentWork.meta.imageHeight}px`}></Image>
-          )
+              <Image onLoad={() => this.imgLoad()} src={currentWork.src} style={`width:${currentWork.meta.imageWidth}px;height:${currentWork.meta.imageHeight}px;`}></Image>
+            )
           }
+          <View className="workImg"></View>
         </View>
         <View className="btnItem">
           <Button type="primary" onClick={this.nextWork}>提交</Button>
