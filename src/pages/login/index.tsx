b15cf2efd08b4a92bf300f6b89aeea71de677e5c
lwang <wanglingsong@gmail.com>
2018-11-07 20:01:55 +0800

    Correct reactjs usage

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,12 @@
-import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text, Input, Button, Image } from '@tarojs/components'
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text, Button, Input, Image } from '@tarojs/components'
 import {save, fetch} from '../../utils/localIfo'
 import { phoneLogin, wechatLogin } from '../../utils/beevalley'
 import './index.scss'
 import wechat from '../../image/wechat.png'
+
 export default class Login extends Component {
+
     constructor(props) {
         super(props)
     }
@@ -86,8 +88,8 @@ export default class Login extends Component {
         return (
             <View className='wrap'>
                 <Text className='title'>用户登录</Text>
-                <input className='inputText' type="text" placeholder='手机号/邮箱/用户名' ref='phone' />
-                <input className='inputText' type='password' placeholder='密码' ref='passWd' />
+                <Input className='inputText' type="text" placeholder='手机号/邮箱/用户名' ref='phone' />
+                <Input className='inputText' type='password' placeholder='密码' ref='passWd' />
                 <Button style="background:orangered;margin-top:3vh;" type="warn" onClick={this.login}>登录</Button>
                 <View className='viewText'>
                     <Text>忘记密码？</Text>
